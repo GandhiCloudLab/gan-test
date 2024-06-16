d426d8f873becf3033d106f5092ea1dbfb65d6c8
Jeya Gandhi Rajan <GandhiCloudLab@gmail.com>
2024-06-17 01:23:20 +0530

    update

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+
+    output: 'standalone',
+
+      // trailingSlash: true,
+      // output: 'standalone',
+      // images: {
+      //     unoptimized: true,
+      //   },
+      //   forceSwcTransforms: true,
+      //   swcMinify: false,
+  
+
+};
+
+module.exports = nextConfig;
---
+++ src/components/common-constants.js
@@ -0,0 +1,10 @@
+// constants.js
+// export const API_URL = 'http://localhost:3001';
+
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001';
+
+// export const API_URL = '';
+
+// export const API_URL = process.env.API_URL;
+
+export const MAX_RESULTS = 10;
